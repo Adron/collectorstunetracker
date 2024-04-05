bd1f6b2541239719935adcee774a391feaf12b19
Adron Hall <adron@thrashingcode.com>
2024-04-05 14:24:30 -0700

    Added a Spring initiliazed Spring Initializr project to this monorepo.

---
+++ api/ctt/src/main/java/org/skidrow/ctt/CttApplication.java
@@ -0,0 +1,13 @@
+package org.skidrow.ctt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CttApplication.class, args);
+	}
+
+}
